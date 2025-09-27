9de038aa8197824ff83678a7e4aa188f30602af5
Hadia Rahmani <140114754+HadiaRahmani@users.noreply.github.com>
2025-09-27 16:04:04 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 function quote() {
   let number = prompt("Choose a number between 1 - 5");
-  if (number == 1) {
-    alert(
-      "“Times of difficulty test our faith, our fortitude and our strength. During these times, the level of our Imaan becomes manifest.” -Yasmin Mogahed, Reclaim Your Heart"
-    );
-  } else if (number == 2) {
-    alert(
-      "“If you seek Him, God can raise you up, and replace the darkness of the ocean, with the light of His Sun.” - Yasmin Mogahed, Reclaim Your Heart"
-    );
-  } else if (number == 3) {
-    alert(
-      "“The greatest victory is to rise from the ashes and rebuild yourself with love and resilience.” - Yasmin Mogahed, Reclaim Your Heart"
-    );
-  } else if (number == 4) {
-    alert(
-      "“You are never alone, for your heart is forever connected to the beating rhythm of the universe.” - Yasmin Mogahed, Reclaim Your Heart"
-    );
-  } else if (number == 5) {
-    alert(
-      "“Sometimes the very thing that breaks you can also heal you.” - Yasmin Mogahed, Reclaim Your Heart"
-    );
+  let message = "";
+
+  switch (number) {
+    case "1":
+      message =
+        "“Times of difficulty test our faith, our fortitude and our strength.” - Yasmin Mogahed, Reclaim Your Heart";
+      break;
+    case "2":
+      message =
+        "“If you seek Him, God can raise you up, and replace the darkness with light.” - Yasmin Mogahed, Reclaim Your Heart";
+      break;
+    case "3":
+      message =
+        "“The greatest victory is to rise from the ashes and rebuild yourself with love.” - Yasmin Mogahed, Reclaim Your Heart";
+      break;
+    case "4":
+      message =
+        "“You are never alone, for your heart is forever connected to the rhythm of the universe.” - Yasmin Mogahed, Reclaim Your Heart";
+      break;
+    case "5":
+      message =
+        "“Sometimes the very thing that breaks you can also heal you.” - Yasmin Mogahed, Reclaim Your Heart";
+      break;
+    default:
+      message = "Please enter a number between 1 and 5!";
   }
+
+  alert(message);
 }
 
-let qouteButton = document.querySelector("button");
-qouteButton.addEventListener("click", quote);
+document.getElementById("quoteBtn").addEventListener("click", quote);
